d0a66a2d9c57e03f46bc1b80cf589245b86ba773
choisunpil <roung4119@gmail.com>
2018-12-08 15:19:42 +0900

    first commit

---
+++ src/main/java/sopt/org/sopstagram/SopstagramApplication.java
@@ -0,0 +1,12 @@
+package sopt.org.sopstagram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SopstagramApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SopstagramApplication.class, args);
+    }
+}
---
+++ src/test/java/sopt/org/sopstagram/SopstagramApplicationTests.java
@@ -0,0 +1,16 @@
+package sopt.org.sopstagram;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SopstagramApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
